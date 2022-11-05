1f1f2d34f109f6e6d74e1ae475e01c201e4ca6de
Eliser14 <elinaa@Elinas-Air.attlocal.net>
2022-11-05 18:50:35 -0500

    act6

--- tests/test_123.py
+++ tests/test_123.py
@@ -5,6 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_eight_components():
+    o = webdriver.ChromeOptions
+    o.headless = True
     driver = webdriver.Chrome(
         service=ChromeService(executable_path=ChromeDriverManager().install())
     )
