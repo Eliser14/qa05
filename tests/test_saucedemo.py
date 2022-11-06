89b90c0096430dd1ee3144b15d025e76dc7881dc
Eliser14 <elinaa@Elinas-Air.attlocal.net>
2022-11-05 22:34:43 -0500

    black .

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
 # from pages.base_page import *
 
 
