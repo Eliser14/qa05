629499726b8bbe1e4555521e1647177776be4264
Eliser14 <elinaa@Elinas-Air.attlocal.net>
2022-11-05 19:26:05 -0500

    act16

--- tests/test_123.py
+++ tests/test_123.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -8,25 +7,11 @@ def test_example():
     o = webdriver.ChromeOptions()
     o.headless = True
 
-    driver = webdriver.Chrome(
-        service=ChromeService(executable_path=ChromeDriverManager().install())
-    )
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
     driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
     title = driver.title
     assert title == "Web form"
-
-    # driver.implicitly_wait(0.5)
-    #
-    # text_box = driver.find_element(by=By.NAME, value="my-text")
-    # submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-    #
-    # text_box.send_keys("Selenium")
-    # submit_button.click()
-    #
-    # message = driver.find_element(by=By.ID, value="message")
-    # value = message.text
-    # assert value == "Received!"
 
     driver.quit()
